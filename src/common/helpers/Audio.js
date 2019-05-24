7ea5d102016646fb5ecc94e7ffe9b5b66dec0172
Lajos Meszaros <m_lajos@hotmail.com>
2019-05-24 15:11:47 +0200

    refactor(Audio): moved sequence data to the constructor and tweaked offset calculation

--- src/common/helpers/Audio.js
+++ src/common/helpers/Audio.js
@@ -6,6 +6,48 @@ import { roundToNDecimals } from './number'
 class Audio extends EventEmitter {
   constructor() {
     super()
+
+    const guitarSequence = [
+      { note: 'D5', dur: '16n', time: `0:0:0` },
+      { note: 'G4', dur: '16n', time: `0:0:2` },
+      { note: 'B4', dur: '8n', time: `0:0:6` },
+      { note: 'F#4', dur: '16n', time: `0:0:12` },
+      { note: 'B3', dur: '16n', time: `0:0:14` },
+      { note: 'E4', dur: '16n', time: `0:0:18` },
+      { note: 'A4', dur: '16n', time: `0:0:20` },
+      { note: 'F#4', dur: '16n', time: `0:0:22` }
+    ]
+
+    const bassSequence1 = [
+      { note: 'A1', dur: '16n', time: `0:0:0` },
+      { note: 'A2', dur: '16n', time: `0:0:4` },
+      { note: 'A1', dur: '16n', time: `0:0:8` },
+      { note: 'A2', dur: '16n', time: `0:0:10` },
+      { note: 'C2', dur: '16n', time: `0:0:16` },
+      { note: 'C3', dur: '16n', time: `0:0:22` },
+      { note: 'C2', dur: '16n', time: `0:0:24` },
+      { note: 'C3', dur: '16n', time: `0:0:28` },
+      { note: 'E1', dur: '16n', time: `0:0:32` },
+      { note: 'E2', dur: '16n', time: `0:0:34` },
+      { note: 'E1', dur: '16n', time: `0:0:40` },
+      { note: 'E2', dur: '16n', time: `0:0:44` }
+    ]
+
+    const bassSequence2 = [
+      { note: 'A1', dur: '16n', time: `0:0:0` },
+      { note: 'A2', dur: '16n', time: `0:0:6` },
+      { note: 'A1', dur: '16n', time: `0:0:8` },
+      { note: 'A2', dur: '16n', time: `0:0:12` },
+      { note: 'C2', dur: '16n', time: `0:0:16` },
+      { note: 'C3', dur: '16n', time: `0:0:18` },
+      { note: 'C2', dur: '16n', time: `0:0:24` },
+      { note: 'C3', dur: '16n', time: `0:0:30` },
+      { note: 'E1', dur: '16n', time: `0:0:32` },
+      { note: 'E2', dur: '16n', time: `0:0:36` },
+      { note: 'E1', dur: '16n', time: `0:0:40` },
+      { note: 'E2', dur: '16n', time: `0:0:42` }
+    ]
+
     this._ = {
       Tone: null,
       bpm: 192,
@@ -13,7 +55,44 @@ class Audio extends EventEmitter {
       duration: '2m',
       parts: {},
       instruments: {},
-      sequences: {}
+      sequences: {
+        guitar1: {
+          instrument: 'guitar1',
+          events: guitarSequence,
+          props: { loop: 2, loopEnd: '1m' },
+          startTime: 0
+        },
+        guitar2: {
+          instrument: 'guitar2',
+          events: guitarSequence,
+          props: { loop: 2, loopEnd: '1m' },
+          startTime: [2, 12]
+        },
+        guitar3: {
+          instrument: 'guitar3',
+          events: guitarSequence,
+          props: { loop: 2, loopEnd: '1m' },
+          startTime: [-3, 12]
+        },
+        guitar4: {
+          instrument: 'guitar4',
+          events: guitarSequence,
+          props: { loop: 2, loopEnd: '1m' },
+          startTime: [5, 12]
+        },
+        bass1: {
+          instrument: 'bass1',
+          events: bassSequence1,
+          props: { loop: 1, loopEnd: '2m' },
+          startTime: 0
+        },
+        bass2: {
+          instrument: 'bass2',
+          events: bassSequence2,
+          props: { loop: 1, loopEnd: '2m' },
+          startTime: 0
+        }
+      }
     }
   }
   isSupported() {
@@ -41,7 +120,7 @@ class Audio extends EventEmitter {
     Tone.Transport.pause()
   }
 
-  calculateOffset(offset, barSize) {
+  calculateOffset([offset, barSize]) {
     const { Tone } = this._
     const x = offset > 0 ? barSize - Math.abs(offset) : Math.abs(offset)
     return -Tone.Time(`0:0:${x * 2}`).toSeconds()
@@ -124,91 +203,17 @@ class Audio extends EventEmitter {
   }
 
   scheduleSong(Transport, loop = false) {
-    // const { sequences } = this._
+    const { sequences } = this._
     Transport.loop = loop
 
-    const guitarSequence = [
-      { note: 'D5', dur: '16n', time: `0:0:0` },
-      { note: 'G4', dur: '16n', time: `0:0:2` },
-      { note: 'B4', dur: '8n', time: `0:0:6` },
-      { note: 'F#4', dur: '16n', time: `0:0:12` },
-      { note: 'B3', dur: '16n', time: `0:0:14` },
-      { note: 'E4', dur: '16n', time: `0:0:18` },
-      { note: 'A4', dur: '16n', time: `0:0:20` },
-      { note: 'F#4', dur: '16n', time: `0:0:22` }
-    ]
-
-    const bassSequence1 = [
-      { note: 'A1', dur: '16n', time: `0:0:0` },
-      { note: 'A2', dur: '16n', time: `0:0:4` },
-      { note: 'A1', dur: '16n', time: `0:0:8` },
-      { note: 'A2', dur: '16n', time: `0:0:10` },
-      { note: 'C2', dur: '16n', time: `0:0:16` },
-      { note: 'C3', dur: '16n', time: `0:0:22` },
-      { note: 'C2', dur: '16n', time: `0:0:24` },
-      { note: 'C3', dur: '16n', time: `0:0:28` },
-      { note: 'E1', dur: '16n', time: `0:0:32` },
-      { note: 'E2', dur: '16n', time: `0:0:34` },
-      { note: 'E1', dur: '16n', time: `0:0:40` },
-      { note: 'E2', dur: '16n', time: `0:0:44` }
-    ]
-
-    const bassSequence2 = [
-      { note: 'A1', dur: '16n', time: `0:0:0` },
-      { note: 'A2', dur: '16n', time: `0:0:6` },
-      { note: 'A1', dur: '16n', time: `0:0:8` },
-      { note: 'A2', dur: '16n', time: `0:0:12` },
-      { note: 'C2', dur: '16n', time: `0:0:16` },
-      { note: 'C3', dur: '16n', time: `0:0:18` },
-      { note: 'C2', dur: '16n', time: `0:0:24` },
-      { note: 'C3', dur: '16n', time: `0:0:30` },
-      { note: 'E1', dur: '16n', time: `0:0:32` },
-      { note: 'E2', dur: '16n', time: `0:0:36` },
-      { note: 'E1', dur: '16n', time: `0:0:40` },
-      { note: 'E2', dur: '16n', time: `0:0:42` }
-    ]
-
-    const sequences = {
-      guitar1: {
-        instrument: 'guitar1',
-        events: guitarSequence,
-        props: { loop: 2, loopEnd: '1m' },
-        startTime: 0
-      },
-      guitar2: {
-        instrument: 'guitar2',
-        events: guitarSequence,
-        props: { loop: 2, loopEnd: '1m' },
-        startTime: this.calculateOffset(2, 12)
-      },
-      guitar3: {
-        instrument: 'guitar3',
-        events: guitarSequence,
-        props: { loop: 2, loopEnd: '1m' },
-        startTime: this.calculateOffset(-3, 12)
-      },
-      guitar4: {
-        instrument: 'guitar4',
-        events: guitarSequence,
-        props: { loop: 2, loopEnd: '1m' },
-        startTime: this.calculateOffset(5, 12)
-      },
-      bass1: {
-        instrument: 'bass1',
-        events: bassSequence1,
-        props: { loop: 1, loopEnd: '2m' },
-        startTime: 0
-      },
-      bass2: {
-        instrument: 'bass2',
-        events: bassSequence2,
-        props: { loop: 1, loopEnd: '2m' },
-        startTime: 0
-      }
-    }
-
     forEach(([name, { instrument, events, props, startTime }]) => {
-      this.setPart(name, instrument, events, props, startTime)
+      this.setPart(
+        name,
+        instrument,
+        events,
+        props,
+        Array.isArray(startTime) ? this.calculateOffset(startTime) : startTime
+      )
     }, toPairs(sequences))
   }
 }
