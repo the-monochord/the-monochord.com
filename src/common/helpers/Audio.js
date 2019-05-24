5010fc60c774b320a652fda787ce0b6d40eed8cc
Lajos Meszaros <m_lajos@hotmail.com>
2019-05-24 13:47:20 +0200

    refactor(Audio): Moved sequence and instrument generation inside the class

--- src/common/helpers/Audio.js
+++ src/common/helpers/Audio.js
@@ -1,149 +1,11 @@
 import EventEmitter from 'eventemitter3'
-// import { reduce, append, forEach, has } from 'ramda'
 import AudioFileManager from 'audio'
+import { forEach, toPairs } from 'ramda'
 import { roundToNDecimals } from './number'
-
-const instruments = {}
-/*
-const sequences = {}
-
-const setSequence = (name, Transport, { sequence, instrument }) => {
-  if (has(name, sequences)) {
-    forEach(event => {
-      Transport.clear(event)
-    }, sequences[name].events)
-  } else {
-    sequences[name] = {
-      events: []
-    }
-  }
-
-  sequences[name].events = reduce((events, { note, dur = 1, startAt }) => {
-    const event = Transport.schedule(time => {
-      instruments[instrument].triggerAttackRelease(note, `${Math.round(16 / dur)}n`, time)
-    }, startAt)
-    return append(event, events)
-  }, [])(sequence)
-}
-*/
-
-const loadInstruments = Tone => {
-  instruments.guitar1 = new Tone.Synth().chain(new Tone.Panner(-0.4), new Tone.Gain(0.5), Tone.Master)
-  instruments.guitar2 = new Tone.Synth().chain(new Tone.Panner(-0.1), new Tone.Gain(0.5), Tone.Master)
-  instruments.guitar3 = new Tone.Synth().chain(new Tone.Panner(0.1), new Tone.Gain(0.5), Tone.Master)
-  instruments.guitar4 = new Tone.Synth().chain(new Tone.Panner(0.4), new Tone.Gain(0.5), Tone.Master)
-
-  instruments.bass1 = new Tone.Synth({ oscillator: { type: 'square' } }).chain(
-    new Tone.Filter(700, 'lowpass'),
-    new Tone.Panner(0.7),
-    new Tone.Gain(0.8),
-    Tone.Master
-  )
-
-  instruments.bass2 = new Tone.Synth({ oscillator: { type: 'square' } }).chain(
-    new Tone.Filter(700, 'lowpass'),
-    new Tone.Panner(-0.7),
-    new Tone.Gain(0.8),
-    Tone.Master
-  )
-}
 
 const calculateOffset = (Tone, offset, barSize) => {
   const x = offset > 0 ? barSize - Math.abs(offset) : Math.abs(offset)
   return -Tone.Time(`0:0:${x * 2}`).toSeconds()
-}
-
-const scheduleSong = (Tone, Transport, loop = false) => {
-  Transport.loop = loop
-
-  const guitarSequence = [
-    { note: 'D5', dur: '16n', time: `0:0:0` },
-    { note: 'G4', dur: '16n', time: `0:0:2` },
-    { note: 'B4', dur: '8n', time: `0:0:6` },
-    { note: 'F#4', dur: '16n', time: `0:0:12` },
-    { note: 'B3', dur: '16n', time: `0:0:14` },
-    { note: 'E4', dur: '16n', time: `0:0:18` },
-    { note: 'A4', dur: '16n', time: `0:0:20` },
-    { note: 'F#4', dur: '16n', time: `0:0:22` }
-  ]
-
-  const guitar1 = new Tone.Part((time, event) => {
-    instruments.guitar1.triggerAttackRelease(event.note, event.dur, time)
-  }, guitarSequence)
-
-  guitar1.loop = 2
-  guitar1.loopEnd = '1m'
-  guitar1.start(0)
-
-  const guitar2 = new Tone.Part((time, event) => {
-    instruments.guitar2.triggerAttackRelease(event.note, event.dur, time)
-  }, guitarSequence)
-
-  guitar2.loop = 2
-  guitar2.loopEnd = '1m'
-  guitar2.start(calculateOffset(Tone, 2, 12)) // rotate right 2
-
-  const guitar3 = new Tone.Part((time, event) => {
-    instruments.guitar3.triggerAttackRelease(event.note, event.dur, time)
-  }, guitarSequence)
-
-  guitar3.loop = 2
-  guitar3.loopEnd = '1m'
-  guitar3.start(calculateOffset(Tone, -3, 12)) // rotate left 3
-
-  const guitar4 = new Tone.Part((time, event) => {
-    instruments.guitar4.triggerAttackRelease(event.note, event.dur, time)
-  }, guitarSequence)
-
-  guitar4.loop = 2
-  guitar4.loopEnd = '1m'
-  guitar4.start(calculateOffset(Tone, 5, 12)) // rotate right 5
-
-  const bassSequence1 = [
-    { note: 'A1', dur: '16n', time: `0:0:0` },
-    { note: 'A2', dur: '16n', time: `0:0:4` },
-    { note: 'A1', dur: '16n', time: `0:0:8` },
-    { note: 'A2', dur: '16n', time: `0:0:10` },
-    { note: 'C2', dur: '16n', time: `0:0:16` },
-    { note: 'C3', dur: '16n', time: `0:0:22` },
-    { note: 'C2', dur: '16n', time: `0:0:24` },
-    { note: 'C3', dur: '16n', time: `0:0:28` },
-    { note: 'E1', dur: '16n', time: `0:0:32` },
-    { note: 'E2', dur: '16n', time: `0:0:34` },
-    { note: 'E1', dur: '16n', time: `0:0:40` },
-    { note: 'E2', dur: '16n', time: `0:0:44` }
-  ]
-
-  const bass1 = new Tone.Part((time, event) => {
-    instruments.bass1.triggerAttackRelease(event.note, event.dur, time)
-  }, bassSequence1)
-
-  bass1.loop = 1
-  bass1.loopEnd = '2m'
-  bass1.start(0)
-
-  const bassSequence2 = [
-    { note: 'A1', dur: '16n', time: `0:0:0` },
-    { note: 'A2', dur: '16n', time: `0:0:6` },
-    { note: 'A1', dur: '16n', time: `0:0:8` },
-    { note: 'A2', dur: '16n', time: `0:0:12` },
-    { note: 'C2', dur: '16n', time: `0:0:16` },
-    { note: 'C3', dur: '16n', time: `0:0:18` },
-    { note: 'C2', dur: '16n', time: `0:0:24` },
-    { note: 'C3', dur: '16n', time: `0:0:30` },
-    { note: 'E1', dur: '16n', time: `0:0:32` },
-    { note: 'E2', dur: '16n', time: `0:0:36` },
-    { note: 'E1', dur: '16n', time: `0:0:40` },
-    { note: 'E2', dur: '16n', time: `0:0:42` }
-  ]
-
-  const bass2 = new Tone.Part((time, event) => {
-    instruments.bass2.triggerAttackRelease(event.note, event.dur, time)
-  }, bassSequence2)
-
-  bass2.loop = 1
-  bass2.loopEnd = '2m'
-  bass2.start(0)
 }
 
 class Audio extends EventEmitter {
@@ -153,7 +15,9 @@ class Audio extends EventEmitter {
       Tone: null,
       bpm: 192,
       timeSignature: [3, 2],
-      duration: '2m'
+      duration: '2m',
+      sequences: {},
+      instruments: {}
     }
   }
   isSupported() {
@@ -167,8 +31,8 @@ class Audio extends EventEmitter {
     this.emit('ready')
 
     this.setTempo()
-    loadInstruments(this._.Tone)
-    scheduleSong(Tone, Tone.Transport, true)
+    this.loadInstruments()
+    this.scheduleSong(Tone.Transport, true)
   }
 
   play() {
@@ -188,6 +52,48 @@ class Audio extends EventEmitter {
     Transport.loopEnd = duration
   }
 
+  loadInstruments() {
+    const { instruments, Tone } = this._
+    instruments.guitar1 = new Tone.Synth().chain(new Tone.Panner(-0.4), new Tone.Gain(0.5), Tone.Master)
+    instruments.guitar2 = new Tone.Synth().chain(new Tone.Panner(-0.1), new Tone.Gain(0.5), Tone.Master)
+    instruments.guitar3 = new Tone.Synth().chain(new Tone.Panner(0.1), new Tone.Gain(0.5), Tone.Master)
+    instruments.guitar4 = new Tone.Synth().chain(new Tone.Panner(0.4), new Tone.Gain(0.5), Tone.Master)
+
+    instruments.bass1 = new Tone.Synth({ oscillator: { type: 'square' } }).chain(
+      new Tone.Filter(700, 'lowpass'),
+      new Tone.Panner(0.7),
+      new Tone.Gain(0.8),
+      Tone.Master
+    )
+
+    instruments.bass2 = new Tone.Synth({ oscillator: { type: 'square' } }).chain(
+      new Tone.Filter(700, 'lowpass'),
+      new Tone.Panner(-0.7),
+      new Tone.Gain(0.8),
+      Tone.Master
+    )
+  }
+
+  setSequence(name, instrument, events, props, startTime) {
+    const { sequences, Tone, instruments } = this._
+
+    if (sequences[name]) {
+      // clear
+    }
+
+    const sequence = new Tone.Part((time, event) => {
+      instruments[instrument].triggerAttackRelease(event.note, event.dur, time)
+    }, events)
+
+    forEach(([key, value]) => {
+      sequence[key] = value
+    }, toPairs(props))
+
+    sequence.start(startTime)
+
+    sequences[name] = sequence
+  }
+
   renderToWav(filename) {
     const { Tone } = this._
 
@@ -196,8 +102,8 @@ class Audio extends EventEmitter {
     Tone.Offline(OfflineTransport => {
       // Tone now points to the offline context
       this.setTempo(OfflineTransport)
-      loadInstruments(this._.Tone)
-      scheduleSong(Tone, OfflineTransport, false)
+      this.loadInstruments()
+      this.scheduleSong(OfflineTransport, false)
 
       OfflineTransport.start()
     }, durationInSeconds).then(buffer => {
@@ -205,9 +111,62 @@ class Audio extends EventEmitter {
 
       // Tone is now back to the normal context, need to reset settings
       this.setTempo()
-      loadInstruments(this._.Tone)
-      scheduleSong(Tone, Tone.Transport, true)
+      this.loadInstruments()
+      this.scheduleSong(Tone.Transport, true)
     })
+  }
+
+  scheduleSong(Transport, loop = false) {
+    const { Tone } = this._
+    Transport.loop = loop
+
+    const guitarSequence = [
+      { note: 'D5', dur: '16n', time: `0:0:0` },
+      { note: 'G4', dur: '16n', time: `0:0:2` },
+      { note: 'B4', dur: '8n', time: `0:0:6` },
+      { note: 'F#4', dur: '16n', time: `0:0:12` },
+      { note: 'B3', dur: '16n', time: `0:0:14` },
+      { note: 'E4', dur: '16n', time: `0:0:18` },
+      { note: 'A4', dur: '16n', time: `0:0:20` },
+      { note: 'F#4', dur: '16n', time: `0:0:22` }
+    ]
+
+    const bassSequence1 = [
+      { note: 'A1', dur: '16n', time: `0:0:0` },
+      { note: 'A2', dur: '16n', time: `0:0:4` },
+      { note: 'A1', dur: '16n', time: `0:0:8` },
+      { note: 'A2', dur: '16n', time: `0:0:10` },
+      { note: 'C2', dur: '16n', time: `0:0:16` },
+      { note: 'C3', dur: '16n', time: `0:0:22` },
+      { note: 'C2', dur: '16n', time: `0:0:24` },
+      { note: 'C3', dur: '16n', time: `0:0:28` },
+      { note: 'E1', dur: '16n', time: `0:0:32` },
+      { note: 'E2', dur: '16n', time: `0:0:34` },
+      { note: 'E1', dur: '16n', time: `0:0:40` },
+      { note: 'E2', dur: '16n', time: `0:0:44` }
+    ]
+
+    const bassSequence2 = [
+      { note: 'A1', dur: '16n', time: `0:0:0` },
+      { note: 'A2', dur: '16n', time: `0:0:6` },
+      { note: 'A1', dur: '16n', time: `0:0:8` },
+      { note: 'A2', dur: '16n', time: `0:0:12` },
+      { note: 'C2', dur: '16n', time: `0:0:16` },
+      { note: 'C3', dur: '16n', time: `0:0:18` },
+      { note: 'C2', dur: '16n', time: `0:0:24` },
+      { note: 'C3', dur: '16n', time: `0:0:30` },
+      { note: 'E1', dur: '16n', time: `0:0:32` },
+      { note: 'E2', dur: '16n', time: `0:0:36` },
+      { note: 'E1', dur: '16n', time: `0:0:40` },
+      { note: 'E2', dur: '16n', time: `0:0:42` }
+    ]
+
+    this.setSequence('guitar1', 'guitar1', guitarSequence, { loop: 2, loopEnd: '1m' }, 0)
+    this.setSequence('guitar2', 'guitar2', guitarSequence, { loop: 2, loopEnd: '1m' }, calculateOffset(Tone, 2, 12))
+    this.setSequence('guitar3', 'guitar3', guitarSequence, { loop: 2, loopEnd: '1m' }, calculateOffset(Tone, -3, 12))
+    this.setSequence('guitar4', 'guitar4', guitarSequence, { loop: 2, loopEnd: '1m' }, calculateOffset(Tone, 5, 12))
+    this.setSequence('bass1', 'bass1', bassSequence1, { loop: 1, loopEnd: '2m' }, 0)
+    this.setSequence('bass2', 'bass2', bassSequence2, { loop: 1, loopEnd: '2m' }, 0)
   }
 }
 
