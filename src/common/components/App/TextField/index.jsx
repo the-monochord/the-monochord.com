5968ec9c10d7be5e4e4e6a18d9d2b4db7ee200cc
Lajos Meszaros <m_lajos@hotmail.com>
2019-06-18 13:08:53 +0200

    feat(components): Added TextField as a replacement for <input type="text" />

---
+++ src/common/components/App/TextField/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextField = props => {
+  return (
+    <div className={'TextField'}>
+      <input {...props} type={'text'} />
+    </div>
+  )
+}
+
+export default TextField
