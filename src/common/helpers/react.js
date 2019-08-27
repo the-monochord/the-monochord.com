20aa2d6c0c693f97344d850b54806d44aa250cf5
Lajos Meszaros <m_lajos@hotmail.com>
2019-08-27 13:52:17 +0200

    feat(react): added isomorphic variation of useSelector() and useDispatch()

--- src/common/helpers/react.js
+++ src/common/helpers/react.js
@@ -1,4 +1,10 @@
+/* global __isBrowser__ */
+
 import { useRef, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import useRouter from 'use-react-router'
+import { compose, prop, pick } from 'ramda'
+import { NOP } from './function'
 
 const useEffectSkipFirst = (fn, inputs) => {
   const isFirstCallRef = useRef(true)
@@ -17,4 +23,29 @@ const useEffectOnce = fn => {
   useEffect(fn, [])
 }
 
-export { useEffectSkipFirst, useEffectOnce }
+const isomorphicUseSelector = selector => {
+  if (__isBrowser__) {
+    return useSelector(selector)
+  } else {
+    const { staticContext } = useRouter()
+    return selector(staticContext)
+  }
+}
+
+const isomorphicUseDispatch = __isBrowser__ ? useDispatch : NOP
+
+const useNamespaceSelector = (namespace, keys) =>
+  isomorphicUseSelector(
+    compose(
+      pick(keys),
+      prop(namespace)
+    )
+  )
+
+export {
+  useEffectSkipFirst,
+  useEffectOnce,
+  isomorphicUseSelector as useSelector,
+  isomorphicUseDispatch as useDispatch,
+  useNamespaceSelector
+}
