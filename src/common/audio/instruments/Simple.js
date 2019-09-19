937d17d5f6e79cadb0f5b8dde135e5d1727a7dc5
Lajos Meszaros <m_lajos@hotmail.com>
2019-09-19 11:49:39 +0200

    fix(audio/simple): fixed envelope for Simple instrument

--- src/common/audio/instruments/Simple.js
+++ src/common/audio/instruments/Simple.js
@@ -48,7 +48,7 @@ class Simple {
     const { nodes, events, ctx } = this._
 
     const attack = 0.01
-    const release = 0.02
+    const release = 0.3
 
     const startFrom = startTime || ctx.currentTime
 
@@ -56,22 +56,24 @@ class Simple {
       const t = startFrom + time
       switch (event) {
         case 'note on':
-          nodes.gain.gain.cancelAndHoldAtTime(t)
-          if (nodes.gain.gain.hasScheduledChanges()) {
+          if (nodes.gain.gain.hasScheduledChangesAtTime(t)) {
+            nodes.gain.gain.cancelAndHoldAtTime(t)
           } else {
             const valueAtTime = nodes.gain.gain.getValueAtTime(t)
             nodes.gain.gain.setValueAtTime(valueAtTime, t)
           }
+
           nodes.gain.gain.linearRampToValueAtTime(velocity, t + attack)
           nodes.oscillator.frequency.setValueAtTime(pitch, t)
           break
         case 'note off':
-          nodes.gain.gain.cancelAndHoldAtTime(t)
-          if (nodes.gain.gain.hasScheduledChanges()) {
+          if (nodes.gain.gain.hasScheduledChangesAtTime(t)) {
+            nodes.gain.gain.cancelAndHoldAtTime(t)
           } else {
             const valueAtTime = nodes.gain.gain.getValueAtTime(t)
             nodes.gain.gain.setValueAtTime(valueAtTime, t)
           }
+
           nodes.gain.gain.linearRampToValueAtTime(0, t + release)
           break
       }
