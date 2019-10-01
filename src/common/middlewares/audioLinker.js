c76c82ebbce2b92f63462af5c194b57c7b1647e3
Lajos Meszaros <m_lajos@hotmail.com>
2019-10-01 17:10:48 +0200

    feat(audiolinker): cursorAt is being updated by an interval, while playing

--- src/common/middlewares/audioLinker.js
+++ src/common/middlewares/audioLinker.js
@@ -1,23 +1,27 @@
-import { find, propEq, isNil, filter, compose, forEach, map, add, flatten, evolve, startsWith, findIndex } from 'ramda'
+import { propEq, isNil, filter, compose, forEach, map, add, flatten, evolve, startsWith, findIndex } from 'ramda'
 import AudioContext from '../contexts/AudioContext'
 import { actions as draftActions } from '../reducers/drafts'
+import { roundToNDecimals } from '../helpers/number'
 // import { actions as stateActions } from '../reducers/state'
 
 const { setCursorPosition } = draftActions
 // const { pauseDraft } = stateActions
 
+let cursorAtInterval = null
+
 const audioLinker = store => next => action => {
   const audio = AudioContext._currentValue
 
+  const result = next(action)
+  const {
+    state: { isPlaying },
+    drafts: { projects }
+  } = store.getState()
+
+  const activeProjectIdx = findIndex(propEq('isActive', true), projects)
+  const activeProject = projects[activeProjectIdx]
+
   if (action.type === 'state/enableAudio' || startsWith('drafts/', action.type)) {
-    const result = next(action)
-    const {
-      state: { isPlaying },
-      drafts: { projects }
-    } = store.getState()
-
-    const activeProject = find(propEq('isActive', true), projects)
-
     if (!isNil(activeProject)) {
       if (isPlaying) {
         // store.dispatch(pauseDraft())
@@ -48,51 +52,63 @@ const audioLinker = store => next => action => {
         )
       )(activeProject.tracks)
     }
-    return result
   } else {
     switch (action.type) {
       case 'state/playDraft':
         {
-          const {
-            drafts: { projects }
-          } = store.getState()
-          const activeProject = find(propEq('isActive', true), projects)
           const cursorAt = isNil(activeProject) ? 0 : activeProject.cursorAt
           audio.play(cursorAt)
+
+          if (cursorAtInterval !== null) {
+            clearInterval(cursorAtInterval)
+          }
+          cursorAtInterval = setInterval(() => {
+            const {
+              drafts: { projects }
+            } = store.getState()
+            const activeProjectIdx = findIndex(propEq('isActive', true), projects)
+            const activeProject = projects[activeProjectIdx]
+            const cursorAt = isNil(activeProject) ? 0 : activeProject.cursorAt
+            // TODO: need to prevent history/add
+            store.dispatch(
+              setCursorPosition({
+                projectIdx: activeProjectIdx,
+                cursorAt: roundToNDecimals(3, cursorAt + 1) // TODO: ctx.currentTime would be more accurate
+              })
+            )
+          }, 500)
         }
         break
       case 'state/pauseDraft':
         {
           const cursorAt = audio.pause()
-          const {
-            drafts: { projects }
-          } = store.getState()
+
+          if (cursorAtInterval !== null) {
+            clearInterval(cursorAtInterval)
+            cursorAtInterval = null
+          }
+
           store.dispatch(
             setCursorPosition({
-              projectIdx: findIndex(propEq('isActive', true), projects),
-              cursorAt: cursorAt
+              projectIdx: activeProjectIdx,
+              cursorAt: roundToNDecimals(3, cursorAt)
             })
           )
         }
         break
       case 'state/stopDraft':
-        {
-          audio.pause()
-          const {
-            drafts: { projects }
-          } = store.getState()
-          store.dispatch(
-            setCursorPosition({
-              projectIdx: findIndex(propEq('isActive', true), projects),
-              cursorAt: 0
-            })
-          )
-        }
+        audio.pause()
+        store.dispatch(
+          setCursorPosition({
+            projectIdx: activeProjectIdx,
+            cursorAt: 0
+          })
+        )
         break
     }
   }
 
-  return next(action)
+  return result
 }
 
 export default audioLinker
