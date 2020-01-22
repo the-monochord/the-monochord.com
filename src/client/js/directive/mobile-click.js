f9b1b2bdd4c7d665f76dc5d6998f8a15799ab8e2
Lajos Meszaros <m_lajos@hotmail.com>
2020-01-22 11:32:27 +0100

    fix(mobile-click): clicking on touch devices should no longer act as double clicks

--- src/client/js/directive/mobile-click.js
+++ src/client/js/directive/mobile-click.js
@@ -8,12 +8,30 @@ angular.module('MobileClick', []).directive('mobileClick', [
     link: (scope, elem, attrs) => {
       const action = $parse(attrs.mobileClick)
 
+      /*
       elem.on('touchstart click', e => {
         e.stopPropagation()
 
         action(scope)
         scope.$apply()
       })
+      */
+
+      if (window.TouchEvent && 'ontouchstart' in elem[0]) {
+        elem.on('touchstart', e => {
+          e.stopPropagation()
+
+          action(scope)
+          scope.$apply()
+        })
+      } else {
+        elem.on('click', e => {
+          e.stopPropagation()
+
+          action(scope)
+          scope.$apply()
+        })
+      }
     }
   })
 ])
