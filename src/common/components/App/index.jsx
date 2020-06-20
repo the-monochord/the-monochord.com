3772444ed95e5fe1d8a179c4badab859ce8b2940
Lajos.Meszaros <m_lajos@hotmail.com>
2020-06-20 06:34:37 +0200

    refactor(components): removed Loading and turned App into a functional component

--- src/common/components/App/index.jsx
+++ src/common/components/App/index.jsx
@@ -1,65 +1,13 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import cn from 'classnames'
-import Loading, { LOADING_STATES } from '../Loading'
 import './style.scss'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = props => {
+  const {
+    data: { theme }
+  } = props
 
-    const { data } = props
-
-    this.state = {
-      ...data,
-      loading: LOADING_STATES.LOADING
-    }
-
-    this.handleLoadingClick = this.handleLoadingClick.bind(this)
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      if (this.state.loading !== LOADING_STATES.LOADED) {
-        this.setState({
-          loading: LOADING_STATES.FADING
-        })
-
-        setTimeout(() => {
-          this.setState({
-            loading: LOADING_STATES.LOADED
-          })
-        }, 1000)
-      }
-    }, 1000)
-  }
-
-  handleLoadingClick() {
-    if (this.state.loading === LOADING_STATES.FADING) {
-      this.setState({
-        loading: LOADING_STATES.LOADED
-      })
-    }
-  }
-
-  render() {
-    // theme should be in redux
-    return (
-      <div className={cn(`theme-${this.state.theme}`)}>
-        The Monochord - now with react
-        <Loading
-          loadingState={this.state.loading}
-          brand={this.state._.seo.brand}
-          splash={this.state._.seo.splash}
-          onClick={this.handleLoadingClick}
-        />
-      </div>
-    )
-  }
-}
-
-App.propTypes = {
-  data: PropTypes.object.isRequired // TODO: turn this into a shape
+  return <div className={cn(`theme-${theme}`)}>The Monochord - now with react</div>
 }
 
 export default App
