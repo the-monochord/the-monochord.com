adb19390b14faa407d1cdb707e1e91eff9a93add
Lajos.Meszaros <m_lajos@hotmail.com>
2020-07-19 19:23:30 +0200

    style(prettier): decreased printWidth from 120 to 100

--- src/client/js/synth/timbres/subtractive/index.js
+++ src/client/js/synth/timbres/subtractive/index.js
@@ -1,4 +1,4 @@
-import { map, compose, objOf, assoc } from 'ramda'
+import { map, compose, objOf, assoc, mergeDeepRight } from 'ramda'
 import AbstractTimbre from '../AbstractTimbre'
 import { sound, change } from './helpers'
 
@@ -15,18 +15,17 @@ import { sound, change } from './helpers'
 class Subtractive extends AbstractTimbre {
   constructor(ctx, wave) {
     super(ctx)
-    this._ = {
-      ...this._,
+    this._ = mergeDeepRight(this._, {
       wave,
       frequencies: [],
       settings: {
-        ...this._.settings,
         mainVolume: 1
       }
-    }
+    })
   }
 
   _render() {
+    console.log('re-rendering:', this._.frequencies, this._.settings)
     change(sound(this._.frequencies, this._.settings, this._.wave), this._.ctx)
   }
 
@@ -43,13 +42,17 @@ class Subtractive extends AbstractTimbre {
   }
 
   noteOn(frequencies) {
-    this._.frequencies = map(compose(assoc('volumes', [{ duration: 1, volume: 1 }]), objOf('frequency')))(frequencies)
+    this._.frequencies = map(
+      compose(assoc('volumes', [{ duration: 1, volume: 1 }]), objOf('frequency'))
+    )(frequencies)
 
     this._render()
   }
 
   noteOff(frequencies) {
-    this._.frequencies = map(compose(assoc('volumes', [{ duration: 20, volume: 0 }]), objOf('frequency')))(frequencies)
+    this._.frequencies = map(
+      compose(assoc('volumes', [{ duration: 20, volume: 0 }]), objOf('frequency'))
+    )(frequencies)
 
     this._render()
   }
