a1e534a9e387a7b9e0ce90579337d021b5bd720e
Mészáros Lajos <m_lajos@hotmail.com>
2021-01-22 01:12:12 +0100

    még egy restart, mert hát kell mégis a vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,82 +1,44 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
+  /*
+  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'The Monochord',
+    titleTemplate: '%s - the-monochord.com',
+    title: 'the-monochord.com',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'The Monochord is an app, which lets you experiment with microtonal scales'
-      },
-
-      { property: 'og:url', content: 'https://the-monochord.com' },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:title', content: 'The Monochord' },
-      {
-        property: 'og:description',
-        content: 'The Monochord is an app, which lets you experiment with microtonal scales'
-      },
-      {
-        property: 'og:image',
-        content: '/img/monochord-logo.png'
-      },
-      { property: 'og:image:type', content: 'image/png' },
-      { property: 'og:image:width', content: '1024' },
-      { property: 'og:image:height', content: '1024' },
-      { name: 'apple-mobile-web-app-title', content: 'The Monochord' },
-      { name: 'application-name', content: 'The Monochord' },
-      { name: 'msapplication-config', content: '/img/browserconfig.xml' },
-      { name: 'theme-color', content: '#33332b' }
+      { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'canonical', href: 'https://the-monochord.com/' },
-      {
-        rel: 'apple-rouch-icon',
-        sizes: '180x180',
-        href: '/img/apple-touch-icon.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/img/favicon-32x32.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/img/favicon-16x16.png'
-      },
-      { rel: 'manifest', href: '/img/manifest.json' },
-      {
-        rel: 'mask-icon',
-        href: '/img/safari-pinned-tab.svg',
-        color: '#33332b'
-      },
-      { rel: 'shortcut icon', href: '/img/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+  */
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
+  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
+  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
     '@nuxtjs/composition-api'
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
+  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
@@ -84,9 +46,28 @@ export default {
     '@nuxtjs/pwa'
   ],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
 }
--- pages/scale-designer.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Scale Designer</h1>
-</template>
